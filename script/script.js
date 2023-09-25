0d16ac4f6860848c546fec34e3e92d9b0f7284c6
Fabien Leroy <leroy223@campus.unicaen.fr>
2023-09-25 12:51:21 +0200

    V1 bien avancée

--- script/script.js
+++ script/script.js
@@ -2,35 +2,68 @@ const inputCP = document.getElementById("inputCP")
 const boutonVille = document.getElementById("boutonVille")
 const inputVille = document.getElementById("inputVille")
 const ville = document.getElementsByClassName("ville")
-const selectVille = document.getElementById("selectVille")
+const resultat = document.getElementById("resultat")
+const formulaire = document.getElementById("formulaire")
+
+const min = document.getElementById("min")
+const max = document.getElementById("max")
+const pluie = document.getElementById("pluie")
+const soleil = document.getElementById("soleil")
 
 let codePostal;
 
 inputCP.addEventListener("input", () => listenerCP())
 boutonVille.addEventListener("click", () => valider())
 
-function listenerCP(){
+
+function listenerCP() {
     codePostal = inputCP.value
+
+    for (var i = 0; i < ville.length; i++) { ville[i].style.display = "none" }
+    resultat.style.display="none"
+
     fetch(`https://geo.api.gouv.fr/communes?codePostal=${codePostal}`)
-    .then(Response => {
-    if(!Response.ok){
-        throw new Error('response not ok');
-    }
-    return Response.json();
-    })
+        .then(Response => {
+            if (!Response.ok) {
+                throw new Error('response not ok');
+            }
+            return Response.json();
+        })
         .then(data => {
-    for(let i = 0; i < data.length; i++){
-        selectVille.innerHTML += `<option value="${data[i]['nom']}">${data[i]['nom']}</option>`
-    }
-    for (var i = 0; i < ville.length; i++) { ville[i].style.display = "block" }
-})
-.catch(error => {
-    console.error('problem', error)
-})
-    
+            if (data.length > 0) {
+                for (let i = 0; i < data.length; i++) {
+                    inputVille.innerHTML += `<option value="${data[i]['nom']}">${data[i]['nom']}</option>`
+                }
+                for (var i = 0; i < ville.length; i++) { ville[i].style.display = "block" }
+            }
+        })
+        .catch(error => {
+            console.error('problem', error)
+        })
 }
-function valider(){
-    console.log("valider")
+
+
+function valider() {
+    resultat.style.display='block'
+    formulaire.style.display='none'
+    fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=4bba169b3e3365061d39563419ab23e5016c0f838ba282498439c41a00ef1091&insee=${inputVille.value}`)
+        .then(Response => {
+            if (!Response.ok) {
+                throw new Error('response not ok');
+            }
+            return Response.json();
+        })
+    .then(data => {
+        if (data > 0) {
+            for (let i = 0; i < data.length; i++) {
+                inputVille.innerHTML += `<option value="${data[i]['nom']}">${data[i]['nom']}</option>`
+            }
+            for (var i = 0; i < ville.length; i++) { ville[i].style.display = "block" }
+        }
+    })
+    .catch(error => {
+        console.error('problem', error)
+    })
 }
 
 
