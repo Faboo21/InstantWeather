04e21d3b2dd68a77093ca23bbfff563f23768cce
Fabien Leroy <leroy223@campus.unicaen.fr>
2023-09-26 11:41:21 +0200

    v1 fonctionne

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ const inputVille = document.getElementById("inputVille")
 const ville = document.getElementsByClassName("ville")
 const resultat = document.getElementById("resultat")
 const formulaire = document.getElementById("formulaire")
+const refresh = document.getElementById('refresh')
 
 const min = document.getElementById("min")
 const max = document.getElementById("max")
@@ -14,7 +15,7 @@ let codePostal;
 
 inputCP.addEventListener("input", () => listenerCP())
 boutonVille.addEventListener("click", () => valider())
-
+refresh.addEventListener("click",() => {location.reload()})
 
 function listenerCP() {
     codePostal = inputCP.value
@@ -25,20 +26,20 @@ function listenerCP() {
     fetch(`https://geo.api.gouv.fr/communes?codePostal=${codePostal}`)
         .then(Response => {
             if (!Response.ok) {
-                throw new Error('response not ok');
+                throw new Error('pas de reponse');
             }
             return Response.json();
         })
         .then(data => {
             if (data.length > 0) {
                 for (let i = 0; i < data.length; i++) {
-                    inputVille.innerHTML += `<option value="${data[i]['nom']}">${data[i]['nom']}</option>`
+                    inputVille.innerHTML += `<option value="${data[i]['code']}">${data[i]['nom']}</option>`
                 }
                 for (var i = 0; i < ville.length; i++) { ville[i].style.display = "block" }
             }
         })
         .catch(error => {
-            console.error('problem', error)
+            console.error('error', error)
         })
 }
 
@@ -46,23 +47,21 @@ function listenerCP() {
 function valider() {
     resultat.style.display='block'
     formulaire.style.display='none'
-    fetch(`https://api.meteo-concept.com/api/forecast/daily/0?token=4bba169b3e3365061d39563419ab23e5016c0f838ba282498439c41a00ef1091&insee=${inputVille.value}`)
+    fetch(`https://api.meteo-concept.com/api/forecast/daily?insee=${inputVille.value}&world=false&start=0&end=0&token=d9588107c8c240cee278bdf86180ba6140aad80ba16953aded2d70d4b5b287ad`)
         .then(Response => {
             if (!Response.ok) {
-                throw new Error('response not ok');
+                throw new Error('pas de reponse');
             }
             return Response.json();
         })
     .then(data => {
-        if (data > 0) {
-            for (let i = 0; i < data.length; i++) {
-                inputVille.innerHTML += `<option value="${data[i]['nom']}">${data[i]['nom']}</option>`
-            }
-            for (var i = 0; i < ville.length; i++) { ville[i].style.display = "block" }
-        }
+        min.innerText=`temperture minimal : ${data['forecast'][0]['tmin']}°C`
+        max.innerText=`temperture maximal : ${data['forecast'][0]['tmax']}°C`
+        soleil.innerText=`Ensoleillement journalier : ${data['forecast'][0]['sun_hours']} heures`
+        pluie.innerText=`Probabilité de pluie : ${data['forecast'][0]['probarain']}%`
     })
     .catch(error => {
-        console.error('problem', error)
+        console.error('error', error)
     })
 }
 
