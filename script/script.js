bcc4a279e23a18e628288a0cab52a9f5bbd9cc7b
Fabien Leroy <leroy223@campus.unicaen.fr>
2023-10-03 11:08:40 +0200

    card en cours

--- script/script.js
+++ script/script.js
@@ -13,6 +13,8 @@ const max = document.getElementById("max")
 const pluie = document.getElementById("pluie")
 const soleil = document.getElementById("soleil")
 const validerParame = document.getElementById("validerParame")
+const weatherCardsContainer = document.getElementById('weatherCardsContainer');
+
 
 const latitude = document.getElementById("latitude")
 const longitude = document.getElementById("longitude")
@@ -26,6 +28,9 @@ const check_vent = document.getElementById("vente");
 const check_dirVent = document.getElementById("dirVente");
 
 let codePostal;
+let numberOfDays = 3;
+let dataWeather;
+
 
 listenerCP()
 inputCP.addEventListener("input", () => listenerCP())
@@ -92,6 +97,13 @@ function valider() {
             precipitation.style.display = 'none'
             vent.style.display = 'none'
             dirVent.style.display = 'none'
+
+            dataWeather = {imagePath: "../images/meteo.png",tempMin: "15°C",tempMax: "21°C"}
+            for (let i = 0; i < numberOfDays; i++) {
+                const weatherCard = new WeatherCard();
+                weatherCard.appendTo(weatherCardsContainer);
+                weatherCard.updateData(dataWeather);
+            }
         })
         .catch(error => {
             console.error('error', error)
@@ -131,3 +143,63 @@ function validerParam() {
     }
 
 }
+
+
+class WeatherCard {
+    constructor() {
+        this.cardElement = this._createCardElement();
+    }
+
+    _createCardElement() {
+        const cardHTML = `
+            <div class="weather-card">
+                <img src="path_to_default_weather_image.jpg" alt="Weather Image" class="weather-image" id="weatherImage">
+                <div class="weather-details">
+                    <!-- ... toutes les autres caractéristiques ... -->
+                </div>
+            </div>
+        `;
+
+        const div = document.createElement('div');
+        div.innerHTML = cardHTML.trim();
+        return div.firstChild;
+    }
+
+    appendTo(parent) {
+        parent.appendChild(this.cardElement);
+    }
+
+    updateData(data) {
+        console.log("hello")
+        if (data.imagePath) this.cardElement.querySelector('#weatherImage').src = data.imagePath;
+        if (data.tempMin) this._updateFeature('tempMin', data.tempMin);
+        if (data.tempMax) this._updateFeature('tempMax', data.tempMax);
+        if (data.rainProb) this._updateFeature('rainProb', data.rainProb);
+        if (data.sunlight) this._updateFeature('sunlight', data.sunlight);
+        if (data.latitude) this._updateFeature('latitude', data.latitude);
+        if (data.longitude) this._updateFeature('longitude', data.longitude);
+        if (data.precipitation) this._updateFeature('precipitation', data.precipitation);
+        if (data.wind) this._updateFeature('wind', data.wind);
+        if (data.windDirection) this._updateFeature('windDirection', data.windDirection);
+    }
+
+    toggleFeature(featureId, shouldShow) {
+        const featureElement = this.cardElement.querySelector(`#${featureId}`);
+        if (featureElement) {
+            featureElement.style.display = shouldShow ? 'block' : 'none';
+        }
+    }
+
+    _updateFeature(featureId, value) {
+        const featureElement = this.cardElement.querySelector(`#${featureId} span`);
+        console.log("Élément à mettre à jour:", featureElement);
+
+        if (featureElement) {
+            featureElement.innerText = value;
+        }
+    }
+}
+
+
+
+
